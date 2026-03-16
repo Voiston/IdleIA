93ae3f2ad24bbab6912788812f8b4558f551a714
VDCoach <david.voisard2@gmail.com>
2026-03-17 00:40:31 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Gestion du cache et des mises à jour forcées
  */
 
-const CACHE_NAME = 'burner-v0.0.99'; // Incrémenté pour forcer la mise à jour
+const CACHE_NAME = 'burner-v0.0.999'; // Incrémenté pour forcer la mise à jour
 const ASSETS = [
   './',
   'index.html',
